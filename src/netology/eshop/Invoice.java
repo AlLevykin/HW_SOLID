d865960dbda4c24e79d9ff925f76cb03ff3037dc
Левыкин Алексей Александрович <alevykin@alfabank.ru>
2025-04-28 17:34:08 +0500

    Суммирование данных в накладной

--- src/netology/eshop/Invoice.java
+++ src/netology/eshop/Invoice.java
@@ -1,8 +1,11 @@
 package netology.eshop;
 
+import java.util.stream.StreamSupport;
+
 public class Invoice {
 
     public static void Print(Iterable<Product> order) {
-        order.forEach(product -> System.out.printf("Ингредиенты: %s, Цена: %.2f\n",product.getDescription(), product.getPrice()));
+        order.forEach(product -> System.out.printf("Ингредиенты: %s, Цена: %.2f\n", product.getDescription(), product.getPrice()));
+        System.out.printf("Итого: %.2f\n", StreamSupport.stream(order.spliterator(), false).mapToDouble(Product::getPrice).sum());
     }
 }
